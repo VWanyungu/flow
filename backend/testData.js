23083c46dc4d392c0dec3f404b1fd511f73d65f0
vicamboka <vicamboka@gmail.com>
2025-12-15 09:19:52 +0300

    feat: add backend folder

---
+++ backend/testData.js
@@ -0,0 +1,75 @@
+const testData = {
+    "songsList": [
+    {
+        "songName": "THOUGHT YOU WERE THE ONE",
+        "artist": "Tommy Richman",
+        "uri": "spotify:track:2E0VJbSnnbLNWHc4OmM3fW"
+    },
+    {
+        "songName": "One Too Many Times",
+        "artist": "Jordan Ward",
+        "uri": "spotify:track:1UcbPy3MrLPE0wU9PYqX54"
+    },
+    {
+        "songName": "CATCH A STRAY",
+        "artist": "Leon Thomas",
+        "uri": "spotify:track:2bMcpeSIM5FNFCv1MK30sQ"
+    },
+    // {
+    //     "songName": "CHERIMOYA - BONUS",
+    //     "artist": "Jordan Ward",
+    //     "uri": "spotify:track:3i4ViOKxBhKpKvHvX8VCtn"
+    // },
+    // {
+    //     "songName": "YUKON",
+    //     "artist": "Justin Bieber",
+    //     "uri": "spotify:track:29iva9idM6rFCPUlu7Rhxl"
+    // },
+    // {
+    //     "songName": "Folded",
+    //     "artist": "Kehlani",
+    //     "uri": "spotify:track:0bxPRWprUVpQK0UFcddkrA"
+    // },
+    // {
+    //     "songName": "Have A Baby (With Me)",
+    //     "artist": "Daniel Caesar",
+    //     "uri": "spotify:track:1V0rl8HIvcfn1ztxPuYMFc"
+    // },
+    // {
+    //     "songName": "MONEY OVER LOVE",
+    //     "artist": "Ywaya Tajiri",
+    //     "uri": "spotify:track:1bRVuXgt09LS1QVUHO4MLT"
+    // },
+    // {
+    //     "songName": "WHITE CROCS (with Ryan Trey)",
+    //     "artist": "Jordan Ward",
+    //     "uri": "spotify:track:7piMQmCeoMwT3jwJcLroN3"
+    // },
+    // {
+    //     "songName": "311 (with Gwen Bunn)",
+    //     "artist": "Jordan Ward",
+    //     "uri": "spotify:track:2K4R3bKsoJV8Rm9I67ddm7"
+    // },
+    // {
+    //     "songName": "melt",
+    //     "artist": "Kehlani",
+    //     "uri": "spotify:track:1pCbBNRglGwlMLeUTOnqrO"
+    // },
+    // {
+    //     "songName": "SUPERPOSITION (feat. John Mayer)",
+    //     "artist": "Daniel Caesar",
+    //     "uri": "spotify:track:1KA2JLeMi0Mo3hjO0442re"
+    // },
+    // {
+    //     "songName": "Oh Lord",
+    //     "artist": "Foxy Shazam",
+    //     "uri": "spotify:track:5iJU96441eAZ2VdWgxdyJa"
+    // },
+    // {
+    //     "songName": "JUICY",
+    //     "artist": "Jordan Ward",
+    //     "uri": "spotify:track:3SYa9xlmRsRB772x9y5GUz"
+    // }
+]
+
+}
